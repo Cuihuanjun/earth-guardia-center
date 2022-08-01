efcc28c1912ed92307832eb36506dc96d5ab2c2a
huanjun.cui <huanjun.cui@lnxall.com>
2022-08-01 11:30:20 +0800

    swagger

--- src/main/java/com/earth/entity/User.java
+++ src/main/java/com/earth/entity/User.java
@@ -2,6 +2,7 @@ package com.earth.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -13,11 +14,24 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @Accessors(chain = true)
 public class User {
+
+//    @JsonSerialize(
+//            using = ToStringSerializer.class
+//    )
+//    @TableId(
+//            value = "id",
+//            type = IdType.ASSIGN_ID
+//    )
+    /**
+     * 主键id
+     */
+    @ApiModelProperty("主键id")
     @TableId(type= IdType.AUTO)
     private Long id;
     private String username;
     private String password;
 
     public User() {
+        // @AllArgsConstructor注解需要空参
     }
 }
