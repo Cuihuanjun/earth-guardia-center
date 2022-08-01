1cfced6dd3f7b3f3ab1af9f2568d97542ef5e1ab
huanjun.cui <huanjun.cui@lnxall.com>
2022-08-01 17:19:32 +0800

    alipay

--- src/main/java/com/earth/base/alipay/AlipayTradeAppPay.java
+++ src/main/java/com/earth/base/alipay/AlipayTradeAppPay.java
@@ -1,4 +1,4 @@
-package com.earth.alipay;
+package com.earth.base.alipay;
 
 import com.alipay.api.AlipayClient;
 import com.alipay.api.AlipayConfig;
@@ -6,12 +6,16 @@ import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.domain.AlipayTradeAppPayModel;
 import com.alipay.api.request.AlipayTradeAppPayRequest;
 import com.alipay.api.response.AlipayTradeAppPayResponse;
+import io.swagger.annotations.ApiModel;
+import org.springframework.beans.factory.annotation.Value;
 
+@ApiModel("阿里提供支付接口20220801")
 public class AlipayTradeAppPay {
-
-    public static void main(String[] args) throws Exception {
-        String privateKey = "<-- 当前帐号未登录，登录后会自动填充当前沙箱帐号的应用私钥 -->";
-        String alipayPublicKey = "<-- 当前帐号未登录，登录后会自动填充当前沙箱帐号的支付宝公钥 -->";
+    @Value("${alipay.privateKey}")
+    private String privateKey; //"<-- 当前帐号未登录，登录后会自动填充当前沙箱帐号的应用私钥 -->";
+    @Value("${alipay.alipayPublicKey}")
+    private String alipayPublicKey;//"<-- 当前帐号未登录，登录后会自动填充当前沙箱帐号的支付宝公钥 -->";
+    public boolean  alipay(AlipayTradeAppPayModel model) throws Exception {
         AlipayConfig alipayConfig = new AlipayConfig();
         alipayConfig.setServerUrl("https://openapi.alipaydev.com/gateway.do");
         //alipayConfig.setAppId("<-- 当前帐号未登录，登录后会自动填充当前沙箱帐号的AppId -->");
@@ -22,10 +26,10 @@ public class AlipayTradeAppPay {
         alipayConfig.setSignType("RSA2");
         AlipayClient alipayClient = new DefaultAlipayClient(alipayConfig);
         AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
-        AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
-        model.setOutTradeNo("70501111111S001111119");
-        model.setTotalAmount("9.00");
-        model.setSubject("大乐透");
+//        AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
+//        model.setOutTradeNo("70501111111S001111119");
+//        model.setTotalAmount("9.00");
+//        model.setSubject("大乐透");
         request.setBizModel(model);
         AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
         System.out.println(response.getBody());
@@ -34,5 +38,6 @@ public class AlipayTradeAppPay {
         } else {
             System.out.println("调用失败");
         }
+        return response.isSuccess();
     }
 }
